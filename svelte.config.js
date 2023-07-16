a01bc82b143def7f7f64b8f094f2604eeed2e1f7
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-07-16 18:25:47 +0100

    wip:

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		inlineStyleThreshold: 5000,
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
